6336e3e825d2013aae47ca6bd99b4752f68f1699
pedro-borga <pedro.borga.santos@gmail.com>
2021-05-20 21:15:18 +0100

    adição CSS e JS

---
+++ js/weconfig.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+    //codigo para manipluação DOM
+    const btnRegister = document.getElementById("btnRegister")
+
+    btnRegister.addEventListener("click", function () {
+    })
+
+
+}
